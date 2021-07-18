6ab4817b03abfc8736648c6b255983bc341adb7b
Chris Chen <ok3201123@gmail.com>
2021-07-18 17:35:12 +0800

    feat: add private messaging with private1.html no history bug

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const helpers = require('./_helpers');
 const cors = require('cors')
-const { Message } = require('./models')
+const { Message, User } = require('./models')
+const { Op } = require("sequelize");
 const app = express()
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -39,14 +40,21 @@ app.use((req, res, next) => {
   next()
 })
 
-
 app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/view/index.html')
 })
 
+app.get('/private1', (req, res, next) => {
+  res.sendFile(__dirname + '/view/private1.html')
+})
+
+app.get('/private2', (req, res, next) => {
+  res.sendFile(__dirname + '/view/private2.html')
+})
+
 // 群聊在線人數
 let onlineCounts = 0
-onlineUser = []
+let onlineUser = []
 
 const publicNamespace = io.of("/public")
 const privateNamespace = io.of("/private")
@@ -80,7 +88,7 @@ publicNamespace.on('connection', async (socket) => {
 
   try {
     // 發送之前的全部訊息
-    msgs = await Message.findAll({
+    const msgs = await Message.findAll({
       raw: true,
       nest: true,
       where: { isPublic: true },
@@ -105,7 +113,7 @@ publicNamespace.on('connection', async (socket) => {
 
       const { content, isPublic } = msg
       // 新訊息放進資料庫
-      let message = await Message.create({
+      const message = await Message.create({
         senderId,
         content,
         isPublic
@@ -113,7 +121,7 @@ publicNamespace.on('connection', async (socket) => {
 
       // 傳新訊息給所有人
       publicNamespace.emit('newMessage', message.toJSON())
-      console.log('message: ' + msg)
+      console.log('message: ', msg)
     } catch (err) {
       console.log(err)
     }
@@ -127,6 +135,95 @@ publicNamespace.on('connection', async (socket) => {
     publicNamespace.emit('online', onlineCounts)
     publicNamespace.emit('onlineUser', onlineUser)
     publicNamespace.emit('userLeave', socket.data.user)
+    console.log('disconnected')
+  })
+})
+
+// -------------- 私人訊息 --------------
+
+// 連線錯誤監聽
+privateNamespace.on("connect_error", (err, next) => {
+  console.log(`connect_error due to ${err.message}`)
+  next(err)
+})
+
+// 連線監聽
+privateNamespace.on('connection', (socket) => {
+  // 接收 current user 回傳 historyMessages array
+  socket.on('currentUser', async (dummyUser) => {
+    try {
+      socket.user = dummyUser
+      console.log('socket.user:', socket.user)
+      socket.join(`${socket.user.id}`)
+      // 找出現在使用者的全部私人訊息
+      const msgs = await Message.findAll({
+        raw: true,
+        nest: true,
+        where: {
+          isPublic: false,
+          [Op.or]: [
+            { senderId: dummyUser.id },
+            { receiverId: dummyUser.id }
+          ]
+        },
+        order: [
+          ['createdAt', 'ASC']
+        ]
+      })
+      // 處理成依私訊對象分組的訊息
+      const messagesPerUser = new Map()
+      msgs.forEach(msg => {
+        const { senderId, receiverId } = msg
+        const otherUserId = socket.user.id === senderId ? receiverId : senderId
+        if (messagesPerUser.has(otherUserId)) {
+          messagesPerUser.get(otherUserId).push(msg)
+        } else {
+          messagesPerUser.set(otherUserId, [msg])
+        }
+      })
+      const users = await User.findAll({
+        raw: true,
+        nest: true,
+        where: { id: { [Op.in]: [...messagesPerUser.keys()] } },
+        attributes: ['id', 'account', 'name', 'avatar']
+      })
+      const messageData = users.map(user => ({
+        to: user,
+        messages: messagesPerUser.get(user.id)
+      }))
+      console.log('messageData: ', messageData)
+      privateNamespace.to(`${socket.user.id}`).emit('historyMessages', messageData)
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  // 私人訊息監聽
+  socket.on('sendMessage', async (msg) => {
+    try {
+      // 前端傳來的訊息為空 return
+      if (!msg.content) return
+
+      const { receiverId, content } = msg
+      // 新訊息放進資料庫
+      const message = await Message.create({
+        senderId: socket.user.id,
+        receiverId,
+        content,
+        isPublic: false
+      })
+
+      // 傳新訊息給所有人
+      privateNamespace.to(`${socket.user.id}`).to(`${receiverId}`).emit('newMessage', message.toJSON())
+      console.log('message: ', message.toJSON())
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  // 離線監聽
+  socket.on('disconnect', () => {
+    // 離開時減少聊天室人數並發送給網頁
     console.log('disconnected')
   })
 })
